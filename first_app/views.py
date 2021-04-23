8dd21372e761e7a9f96323e9e261afd38126c370
Your Name <stevenbeltz@ymail.com>
2021-04-23 18:44:39 -0500

    revised survey

--- first_app/views.py
+++ first_app/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'index.html')
+def revised(request):
+    # context = {
+    #     'name' = request.session["name"]
+    #     'location' = request.session["location"]
+    #     'language' = request.session["language"]
+    #     'comment' = request.session["comment"]
+    # }
+    
+    return render(request,'results.html')
 def results(request):
     if request.method == "POST":
-        context = {
-            "name": request.POST["name"],
-            "location": request.POST["location"],
-            "language": request.POST["language"],
-            "comment": request.POST["comment"],
-        }
-    return render(request, 'results.html', context)
+        request.session["name"] = request.POST["name"]
+        request.session["location"] = request.POST["location"]
+        request.session["language"] = request.POST["language"]
+        request.session["comment"] = request.POST["comment"]
+    return redirect('/revised')
